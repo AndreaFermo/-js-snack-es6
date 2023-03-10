45693f80776a9ea7d768ae943cc1eef5b1f3c640
Andrea Fermo <andreafermo.sd@gmail.com>
2023-03-10 14:12:00 +0100

    snack1

---
+++ snack1/js/main.js
@@ -0,0 +1,34 @@
+/**********************
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+***********************/
+
+const bici = [
+    {
+        nome:'bianchi',
+        peso:36
+    },
+    {
+        nome:'atala',
+        peso:25
+    },
+    {
+        nome:'olmo',
+        peso:45
+    }
+];
+
+let pesoBiciPiuLeggera = Infinity;
+let nomeBiciPiuLeggera;
+
+bici.forEach((elemento) => {
+    const {nome, peso} = elemento;
+    if (peso < pesoBiciPiuLeggera){
+        pesoBiciPiuLeggera = peso;
+        nomeBiciPiuLeggera = nome;
+    } 
+});
+
+console.log(`${nomeBiciPiuLeggera}`)
